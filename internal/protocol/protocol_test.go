bb039ce0035aefa8d0288d3b31f135f56f435760
jto05 <julian.to2023@gmail.com>
2026-01-12 18:01:54 -0800

    Added basic test file for protocol

---
+++ internal/protocol/protocol_test.go
@@ -0,0 +1,16 @@
+package protocol_test
+
+import (
+	"testing"
+
+	"beam-fs/internal/protocol"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestProtocol_ParseArgs(t *testing.T) {
+	t.Parallel()
+	type TestCase struct {
+		got  string
+		want protocol.Command
+	}
+}
